038c6d89d1e5d21409a08857808747032dde79c0
Yossi Zahn <yossizahn@gmail.com>
2022-02-23 12:42:10 +0200

    Convert to scala2 & fix warnings

--- build.sbt
+++ build.sbt
@@ -1,7 +1,9 @@
-scalaVersion := "3.1.0"
+scalaVersion := "2.13.8"
 enablePlugins(ScalaJSPlugin)
 scalaJSUseMainModuleInitializer := true
 
 libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "2.0.0"
 libraryDependencies += "dev.zio" %%% "zio" % "1.0.12"
 libraryDependencies += "io.github.cquiroz" %%% "scala-java-time" % "2.3.0"
+
+scalacOptions += "-Xsource:3"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -14,9 +14,9 @@ case class Game(
 )
 
 object Main extends App {
-  val statusDisplay = document.querySelector(".game--status")
+  private val statusDisplay = document.querySelector(".game--status")
 
-  val initialState = Game(true, "X", List("", "", "", "", "", "", "", "", ""))
+  private val initialState = Game(active = true, "X", List("", "", "", "", "", "", "", "", ""))
 
   def winningMessage(game: Game) = s"Player ${game.currentPlayer} has won!"
   def drawMessage = s"Game ended in a draw!"
@@ -33,7 +33,7 @@ object Main extends App {
     List(2, 4, 6)
   )
 
-  def handleCellPlayed(
+  private def handleCellPlayed(
       game: Game
   )(clickedCell: Element, clickedCellIndex: Int) = {
     val game2 = game.copy(cells =
@@ -43,14 +43,14 @@ object Main extends App {
     game2
   }
 
-  def handlePlayerChange(game: Game) = {
+  private def handlePlayerChange(game: Game) = {
     val game2 =
       game.copy(currentPlayer = if (game.currentPlayer == "X") "O" else "X")
     statusDisplay.innerHTML = currentPlayerTurn(game2)
     game2
   }
 
-  def isWin(game: Game)(winCondition: List[Int]) =
+  private def isWin(game: Game)(winCondition: List[Int]) =
     winCondition.map(game.cells) match {
       case List("X", "X", "X") | List("O", "O", "O") => true
       case _                                         => false
@@ -81,14 +81,14 @@ object Main extends App {
     } else game
   }
 
-  def handleRestartGame = UIO {
+  private def handleRestartGame = UIO {
     val game = initialState
     statusDisplay.innerHTML = currentPlayerTurn(game)
     document.querySelectorAll(".cell").foreach(cell => cell.innerHTML = "")
     game
   }
 
-  extension (element: Element)
+  implicit class ElementExt(element: Element) {
     def addClickListener(f: Event => UIO[Unit]): UIO[Unit] =
       UIO {
         element.addEventListener(
@@ -96,22 +96,24 @@ object Main extends App {
           event => Runtime.global.unsafeRunAsync_(f(event))
         )
       }
+  }
 
-  def run(args: List[String]): URIO[ZEnv, ExitCode] =
+  def run(args: List[String]): URIO[ZEnv, ExitCode] = {
     var _game = initialState
 
     statusDisplay.innerHTML = currentPlayerTurn(_game)
 
-    def updateGame(game: Game) = UIO { _game = game }
+    def updateGame(game: Game) = UIO {
+      _game = game
+    }
 
     ZIO
-      .foreach(document.querySelectorAll(".cell"))(
+      .foreach_(document.querySelectorAll(".cell")) {
         _.addClickListener(handleCellClick(_game)(_).flatMap(updateGame))
-      )
-      .flatMap { _ =>
-        document
-          .querySelector(".game--restart")
-          .addClickListener(_ => handleRestartGame.flatMap(updateGame))
-      }
-      .as(ExitCode.success)
+      } *> {
+      document
+        .querySelector(".game--restart")
+        .addClickListener(_ => handleRestartGame.flatMap(updateGame))
+    }.exitCode
+  }
 }
